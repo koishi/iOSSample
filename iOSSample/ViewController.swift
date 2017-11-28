f2c0e4e80e730a135a97943bbf431f5fd4d59a51
Koichiro Oishi <k.oishi@manabo.com>
2017-11-28 17:05:15 +0900

    use Storyboard

--- iOSSample/ViewController.swift
+++ iOSSample/ViewController.swift
@@ -8,41 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate {
-    
-    var scrollView: UIScrollView!
-    var pageControll: UIPageControl!
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControll: UIPageControl!
     let pageNum = 4
     let pageColors:[Int : UIColor] = [1 : UIColor.red, 2 : UIColor.yellow, 3 : UIColor.blue, 4 : UIColor.green]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        scrollView = UIScrollView(frame: view.bounds)
-        scrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(pageNum), height: view.bounds.height)
+
+        scrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(pageNum), height: 200)
         scrollView.isPagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.delegate = self;
-        view.addSubview(scrollView)
-        
-        pageControll = UIPageControl(frame: CGRect(x: 0, y: view.bounds.height-50, width: view.bounds.width, height: 50))
+
         pageControll.numberOfPages = pageNum
         pageControll.currentPage = 0
-        view.addSubview(pageControll)
-        
+
         for p in 1...pageNum {
-            let v = UIView(frame: CGRect(x: view.bounds.width * CGFloat(p-1), y: 0, width: view.bounds.width, height: view.bounds.height))
+            let v = UIView(frame: CGRect(x: view.bounds.width * CGFloat(p-1), y: 0, width: view.bounds.width, height: scrollView.bounds.height))
             v.backgroundColor = pageColors[p]
             self.scrollView.addSubview(v)
         }
     }
-    
+}
+
+extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageProgress = Double(scrollView.contentOffset.x / scrollView.bounds.width)
         pageControll.currentPage = Int(round(pageProgress))
-        
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 }
